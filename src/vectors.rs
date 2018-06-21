0a372f70e427b85d826218f22cde5693695fa758
Dan Reeves <hey@danreev.es>
2018-06-21 09:01:23 +0100

    more vectors

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,7 +1,13 @@
 #[test]
-fn vectors() {
+fn vector_macro() {
     let x = vec![0; 10];
     assert_eq!(x, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+}
+
+#[test]
+fn vector_constructor() {
+    let x: Vec<i32> = Vec::new();
+    assert_eq!(x, vec![]);
 }
 
 #[test]
@@ -11,7 +17,7 @@ fn index_access() {
 }
 
 #[test]
-fn out_of_bounds_access() {
+fn maybe_out_of_bounds_access() {
     let x = vec![1, 2, 3];
     let maybe_index = x.get(42);
     if let Some(_val) = maybe_index {
@@ -19,4 +25,42 @@ fn out_of_bounds_access() {
     } else {
         assert!(true);
     }
+}
+
+#[test]
+fn push() {
+    let mut x: Vec<i32> = Vec::new();
+    assert_eq!(x, vec![]);
+    x.push(1);
+    assert_eq!(x, vec![1]);
+}
+
+#[test]
+fn pop() {
+    let mut x = vec![1, 2, 3];
+    let maybe_three = x.pop();
+    if let Some(three) = maybe_three {
+        assert_eq!(three, 3);
+    }
+    assert_eq!(x, vec![1, 2]);
+}
+
+#[test]
+fn iterating() {
+    let x = vec![1, 2, 3];
+    let mut count = 0;
+    for _i in &x {
+        count += 1
+    }
+    assert_eq!(count, 3);
+}
+
+#[test]
+fn mutably_iterating() {
+    let mut x = vec![1, 2, 3];
+    for i in &mut x {
+        // Need to dereference the pointer to an integer
+        *i *= 10
+    }
+    assert_eq!(x, vec![10, 20, 30]);
 }
