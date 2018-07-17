57fce7caf08d24895cc8fc8ac1c5f52a38802e4c
Dan Reeves <hey@danreev.es>
2018-07-18 00:09:08 +0100

    implementing iterators

--- src/iterators.rs
+++ src/iterators.rs
@@ -19,3 +19,46 @@ fn into_iter() {
     assert_eq!(it.next(), Some(3));
     assert_eq!(it.next(), None);
 }
+
+#[test]
+fn map() {
+    let a: Vec<i32> = vec![1, 2, 3];
+    let b: Vec<i32> = a.iter().map(|x| x + 1).collect();
+    assert_eq!(b, vec![2, 3, 4]);
+}
+
+#[test]
+fn implementing_iterator() {
+    struct Counter {
+        count: u32,
+        max_num: u32,
+    };
+
+    impl Counter {
+        fn new(max_num: u32) -> Counter {
+            Counter { max_num, count: 0 }
+        }
+    };
+
+    // Here's the good stuff
+    impl Iterator for Counter {
+        type Item = u32;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.count += 1;
+
+            if self.count <= self.max_num {
+                Some(self.count)
+            } else {
+                None
+            }
+        }
+    }
+
+    let counter = Counter::new(5);
+    let mut list: Vec<u32> = Vec::new();
+    for i in counter {
+        list.push(i);
+    }
+    assert_eq!(list, vec![1, 2, 3, 4, 5]);
+}
